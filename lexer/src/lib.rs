83892638bead09cdc663264757f29e1e0e6ca511
darkdrag00nv2 <darkdrag00n@rambler.ru>
2023-01-18 21:36:05 +0530

    Implement token for lexer

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod token;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Clone, Debug)]
+pub struct Lexer {
+    pub code: String,
 }
